964850a3a856a193c5abe662bccda4c65577134e
Fabrizzio Arguello <fabrez.argue@hotmail.com>
2025-02-17 02:01:05 -0500

    Trying to find BER from looped SNR

--- 1000_Bit_Stream_Error.py
+++ 1000_Bit_Stream_Error.py
@@ -10,31 +10,46 @@
 
 #Libraries Used
 import numpy as np
+import pandas as pd
+import openpyxl as xl
 
 #Constants
-BIT_AMOUNT = 1000 #size of bit stream
-POWER = 10**-7 #excpeted POWER output from signal
-TRIALS = 1000 #total number of trials
+BIT_AMOUNT = 100                                         #size of bit stream
+POWER = 10**-7                                             #excpeted POWER output from signal
+TRIALS = 1000                                              #total number of trials
 
 error_rate = [] #Create empty list
+t_sig_list = []
+r_sig_list = []
 
-for val in range(TRIALS):
+
+SNR = 70
+
+for val in range(0,SNR,1):
 
     bits = np.random.randint(0, 2, BIT_AMOUNT)   # A random bit array is made based on bit amount
+    print(bits)
 
     voltage = np.sqrt(POWER)                               # Voltage received from POWER output of signal
 
     threshold = voltage / 2                                # For simplistic reason threshold will be half the voltage
                                                            # for detection
 
-    t_sig = bits * voltage                                 # transmitted signal with correct amplitude
+    t_sig=(bits * voltage)    # transmitted signal with correct amplitude
+    print(t_sig)
+
+
+    channel = np.sqrt(val)
+    print(channel)
+
 
     #Generate an array of noise values
-    noise_var = POWER / 10                                 # Variance of noise signal
-    noise = np.random.normal(0, np.sqrt(noise_var), BIT_AMOUNT)  #array made with mean = 0, variance, array size
+    noise_var = POWER                       # Variance of noise signal
+    noise = np.random.normal(0,noise_var, BIT_AMOUNT)  #array made with mean = 0, variance, array size
 
     #Received Signal function with signal amplitude and added noise
-    r_sig = t_sig + noise
+    r_sig = channel*t_sig + noise
+    print(r_sig)
 
     decoded_bits = []                                       #Create an empty list
     error_cnt = 0                                           #Initialize error count
@@ -49,9 +64,16 @@ for val in range(TRIALS):
         if decoded_bits[i] != bits[i]:
             error_cnt += 1                                  #count the error of decoded bits that don't match bit stream
 
+    t_sig_list.append(t_sig)
+    r_sig_list.append(r_sig)
+
     #Error count list
-    error_rate.append((error_cnt / BIT_AMOUNT) * 100)
+    error_rate.append((error_cnt / BIT_AMOUNT))
 
 #Results:
+dt = pd.DataFrame(t_sig_list).T
+dt.to_excel('signal.xlsx',index=False, sheet_name='Signal')
+dr = pd.DataFrame(r_sig_list).T
+dr.to_excel('real.xlsx',index=False, sheet_name='Real')
 print(f'\n{error_rate}')
 print(f'\nAverage Percentage Error of Signal Transmission: {np.mean(error_rate):.3f}%')
